249e1ffd9a0f86054efd233f2faa769805ac1f8c
takhimi <takhimi@gmail.com>
2017-03-24 11:23:25 +0800

    Added something

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,6 +7,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("You found the sousage");
+        print("something changed");
 	}
 	
 	// Update is called once per frame
